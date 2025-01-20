202a8785295e0e3abe166c6ffe4482d3bd67a3c2
Dulina Senarathne <dulinahansa2002@gmail.com>
2025-01-20 17:11:51 +0530

    minor update

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class GrowMateApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'GrowMate-Plant Companion',
+      title: 'GrowMate-Plant Companion!!',
       theme: ThemeData(primarySwatch: Colors.green),
       home: LoginPage(userAccounts: userAccounts),
     );
